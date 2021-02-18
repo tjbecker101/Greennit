a9672bc4a4084f7ca21494a696b2507e9bba07c8
BuildTools <unconfigured@null.spigotmc.org>
2021-02-18 13:55:17 -0500

    Modified initial commit

---
+++ src/main/java/com/greennit/CS3141/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.greennit.CS3141;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class HelloApplication extends Application {
+
+}
