3c9724954412eb00eee91fe9526dafa2047aa454
Tyler Becker <77254898+tjbecker101@users.noreply.github.com>
2021-02-26 02:26:04 -0500

    Created a class that would allow us to get data from the database, however the connection is not working yet. Feel free to try and debug it yourself.

--- src/main/java/com/greennit/CS3141/User.java
+++ src/main/java/com/greennit/CS3141/User.java
@@ -1,19 +1,44 @@
 package com.greennit.CS3141;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-public class User {
+/**
+ * This user class allows one to create posts, edit or delete existing posts, as well as create a new subgreenit.
+ * The user can also logout, change password, or delete their account with this class
+ */
+public class User{
+
     private String username;
     private int karma;
     private String password;
 
-    private void createPost(){
+    private User(String username, String password){
+        this.username = username;
+        karma = getKarma(username);
+        this.password = password;
+    }
+
+    /**
+     * Gets the karma tied to the username
+     * @param username - key for the database to get the karma
+     * @return - karma for the user
+     */
+    private int getKarma(String username){
+        return 0;
+    }
+
+    private void createPost(Post post){
 
     }
 
-    private void editPost(){
+    private void editPost(Post post){
 
     }
 
-    private void deletePost() {
+    private void deletePost(Post post) {
 
     }
 
