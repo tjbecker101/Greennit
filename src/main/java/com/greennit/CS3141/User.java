fc7406cb606526f8b13eb384db9b123915c6b699
Tyler Becker <77254898+tjbecker101@users.noreply.github.com>
2021-02-25 19:14:48 -0500

    Blank methods and variables

--- src/main/java/com/greennit/CS3141/User.java
+++ src/main/java/com/greennit/CS3141/User.java
@@ -1,4 +1,36 @@
 package com.greennit.CS3141;
 
 public class User {
+    private String username;
+    private int karma;
+    private String password;
+
+    private void createPost(){
+
+    }
+
+    private void editPost(){
+
+    }
+
+    private void deletePost() {
+
+    }
+
+    private void createSubgreenit(String name){
+
+    }
+
+    private String changePassword(String password){
+        return null;
+    }
+
+    private void deleteAccount(){
+
+    }
+
+    private void logout(){
+
+    }
+
 }
