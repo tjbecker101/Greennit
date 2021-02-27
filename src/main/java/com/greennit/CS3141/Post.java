82ffbc9feafe525be5a5733bd63f1677891b7a5f
BuildTools <unconfigured@null.spigotmc.org>
2021-02-27 16:06:38 -0500

    Blank method signitures added

--- src/main/java/com/greennit/CS3141/Post.java
+++ src/main/java/com/greennit/CS3141/Post.java
@@ -1,4 +1,32 @@
 package com.greennit.CS3141;
 
+import java.time.LocalDateTime;
+
 public class Post {
+    private String content;
+    private String author;
+    private int likes;
+    private LocalDateTime timePosted;
+
+    public Post(String content, String author) {
+        this.content = content;
+        this.author = author;
+        likes = 0;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public LocalDateTime getTimePosted() {
+        return timePosted;
+    }
 }
