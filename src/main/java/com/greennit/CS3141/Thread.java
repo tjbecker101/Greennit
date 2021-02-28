90075bfa2f35ced3c1d19974e2d4e770072b1e5c
BuildTools <unconfigured@null.spigotmc.org>
2021-02-27 19:30:50 -0500

    Added Hibernate support for threads

--- src/main/java/com/greennit/CS3141/Thread.java
+++ src/main/java/com/greennit/CS3141/Thread.java
@@ -1,4 +1,79 @@
 package com.greennit.CS3141;
 
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "threads")
+/*
+ * This thread class is used for inserting and retrieving data from a thread such as:
+ * The subgreennit the thread is on, the id of the thread, the title and author of the thread,
+ * the content of the thread, and the time the thread was created.
+ */
 public class Thread {
+    @Id
+    @Column(name = "host_subgreennit")
+    private int host_subgreennit;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "thread_id")
+    private int thread_id;
+
+    private String title;
+    private String author;
+    private String content;
+    private Timestamp creation_date;
+
+    public Thread() {
+    }
+
+    // region getters and setters
+
+    public int getHost_subgreennit() {
+        return host_subgreennit;
+    }
+
+    public void setHost_subgreennit(int host_subgreennit) {
+        this.host_subgreennit = host_subgreennit;
+    }
+
+    public int getThread_id() {
+        return thread_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getCreation_date() {
+        return creation_date;
+    }
+
+    public void setCreation_date(Timestamp creation_date) {
+        this.creation_date = creation_date;
+    }
+
+    // endregion
+
 }
