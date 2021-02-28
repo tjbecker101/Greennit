424a31b68d21322ee246d06c3a441b130685f4fa
BuildTools <unconfigured@null.spigotmc.org>
2021-02-27 19:32:22 -0500

    Added Hibernate support for threads

--- src/main/java/com/greennit/CS3141/Post.java
+++ src/main/java/com/greennit/CS3141/Post.java
@@ -1,32 +1,80 @@
 package com.greennit.CS3141;
 
-import java.time.LocalDateTime;
+import javax.persistence.*;
+import java.sql.Timestamp;
 
+@Entity
+@Table(name = "posts")
+/*
+ * This post class is used for inserting and retrieving data from a post such as:
+ * The thread the post belongs to, the id of the post, the author and content of the post,
+ * the time the post was created, and how many likes the post has.
+ */
 public class Post {
-    private String content;
-    private String author;
-    private int likes;
-    private LocalDateTime timePosted;
+    @Id
+    @Column(name = "host_thread")
+    private int host_thread;
 
-    public Post(String content, String author) {
-        this.content = content;
-        this.author = author;
-        likes = 0;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="post_id")
+    private int post_id;
+
+    private String author;
+    private String content;
+    private Timestamp creation_date;
+    private int likes;
+
+    public Post() {
     }
 
-    public String getContent() {
-        return content;
+    // region getters and setters
+    public int getHost_thread() {
+        return host_thread;
+    }
+
+    public void setHost_thread(int host_thread) {
+        this.host_thread = host_thread;
+    }
+
+
+    public int getPost_id() {
+        return post_id;
     }
 
     public String getAuthor() {
         return author;
     }
 
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getCreation_date() {
+        return creation_date;
+    }
+
+    public void setCreation_date(Timestamp creation_date) {
+        this.creation_date = creation_date;
+    }
+
     public int getLikes() {
         return likes;
     }
 
-    public LocalDateTime getTimePosted() {
-        return timePosted;
+    public void setLikes(int likes) {
+        this.likes = likes;
     }
+
+    // endregion
+
+
 }
