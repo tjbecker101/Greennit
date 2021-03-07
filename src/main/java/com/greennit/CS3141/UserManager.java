b488ed19dd0f7e9ab2f583b5005afc5831d828ad
Tyler Becker <77254898+tjbecker101@users.noreply.github.com>
2021-03-07 13:29:12 -0500

    Added Comments to User and UserManager

--- src/main/java/com/greennit/CS3141/UserManager.java
+++ src/main/java/com/greennit/CS3141/UserManager.java
@@ -5,13 +5,15 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
 
 public class UserManager {
 
     private SessionFactory sessionFactory;
     private Session session;
 
+    /**
+     * Creates a connection with the database using hibernate
+     */
     public void setup() {
         StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .configure("META-INF/hibernate.cfg.xml")
@@ -24,33 +26,48 @@ public class UserManager {
         }
     }
 
+    /**
+     * Closes the open connection to the database
+     */
     public void exit() {
         sessionFactory.close();
     }
 
-    public void create(String username, String password, String email, int karma, int permission) {
+    /**
+     * Used to create a new entry in the database
+     * @param username - the username of the new user
+     * @param password - the password of the new user
+     * @param email - the email of the new user
+     */
+    public void create(String username, String password, String email) {
+        //Creates a user with the provided values
         User user = new User();
         user.setUsername(username);
-        user.setPermission(permission);
-        user.setKarma(karma);
+        user.setPermission(1);
+        user.setKarma(0);
         user.setEmail(email);
         user.setPass(password);
 
+        //Takes the user we created and puts it into the database as an entry
         session = sessionFactory.openSession();
         session.beginTransaction();
-
         session.update(user);
-
         session.getTransaction().commit();
         session.close();
     }
 
+    /**
+     * This will search the database for a user from their username
+     * @param username - used as the primary key in the table and helps us locate the entry in the table
+     * @return - returns a user which we can then use to get the information we need
+     * @throws IllegalArgumentException - thrown if the user isn't in the table
+     */
     public User getUser(String username) throws IllegalArgumentException {
-        session = sessionFactory.openSession();
 
+        session = sessionFactory.openSession();
         User user = session.get(User.class, username);
 
-        if (user.getUsername() == null || user.getUsername().equals("")) {
+        if (user.getUsername() == null || user.getUsername().equals("")) { //Checks if the username exists or not
             throw new IllegalArgumentException("Username provided not valid.");
         }
 
@@ -58,19 +75,24 @@ public class UserManager {
         return user;
     }
 
+    /**
+     * Used to update a value already in the table, such as changing username, or password, or updating their karma
+     */
     public void update() {
 
     }
 
+    /**
+     * Will remove a user from the table and as a result delete their account
+     * @param username - the username of the one to be deleted
+     */
     public void delete(String username) {
+
         User user = new User();
         user.setUsername(username);
-
         session = sessionFactory.openSession();
         session.beginTransaction();
-
         session.delete(user);
-
         session.getTransaction().commit();
         session.close();
 
