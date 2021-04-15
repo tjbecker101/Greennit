eeb39a579e5853e0d35dc578716fc14f05d23dba
Quentin Ross <quentin@familyross.net>
2021-04-14 23:37:42 -0400

    Fixed small UI bug where fail messages weren't showing.

--- src/main/java/com/greennit/CS3141/webpage/UserSignupServlet.java
+++ src/main/java/com/greennit/CS3141/webpage/UserSignupServlet.java
@@ -74,17 +74,17 @@ public class UserSignupServlet extends HttpServlet {
             isValid = false;
         } catch (IllegalArgumentException ignored) {
         }
-        if (!isValidEmail(email) && isValid) {
+        if (!isValidEmail(email)) {
             String messageEmail = "Invalid email address";
             request.setAttribute("messageEmail", messageEmail);
             isValid = false;
         }
-        if (!isValidPassword(password1) && isValid) {
+        if (!isValidPassword(password1)) {
             String messageBadPassword = "Invalid password parameters";
             request.setAttribute("messageBadPassword", messageBadPassword);
             isValid = false;
         }
-        if (!passwordsMatch(password1, password2) && isValid) {
+        if (!passwordsMatch(password1, password2)) {
             String messagePasswordMismatch = "Passwords do not match";
             request.setAttribute("messagePasswordMismatch", messagePasswordMismatch);
             isValid = false;
