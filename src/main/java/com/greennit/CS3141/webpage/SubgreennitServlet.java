ec64954451f289d780533db52aa9bf93013c2f2f
Quentin Ross <quentin@familyross.net>
2021-04-18 19:03:25 -0400

    Sorted threads by most recent first.

--- src/main/java/com/greennit/CS3141/webpage/SubgreennitServlet.java
+++ src/main/java/com/greennit/CS3141/webpage/SubgreennitServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 @WebServlet("/subgreennit")
@@ -40,6 +41,7 @@ public class SubgreennitServlet extends HttpServlet {
         } else {
             subgreennit = subgreennitManager.getSubgreennit(id);
             List<Thread> threadsInSubgreennit = threadManager.getThreadsBySubgreennit(subgreennit.getId());
+            Collections.reverse(threadsInSubgreennit);
             HttpSession session = request.getSession();
             session.setAttribute("currentSubgreennit", subgreennit);
             session.setAttribute("threadsInSubgreennit", threadsInSubgreennit);
