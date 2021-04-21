d3ab7de87d64b4d8830470fd89e13a290f66880e
Quentin Ross <quentin@familyross.net>
2021-04-21 19:39:53 -0400

    Removed old page and classes.

--- src/main/java/com/greennit/CS3141/testPage/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package com.greennit.CS3141.testPage;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class HelloApplication extends Application {
-
-}
